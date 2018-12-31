44ed1af4e3911837996541b1836cb252920038ee
tmratwork <tmratwork@gmail.com>
2018-12-30 23:46:21 -0800

    added haptic feedback to corner tap gestures

--- Build/Android/src/com/YourCompany/VorailSK/OBBData.java
+++ Build/Android/src/com/YourCompany/VorailSK/OBBData.java
@@ -19,7 +19,7 @@ public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
 "4", // the version of the APK that the file was uploaded against
-139349777L // the length of the file in bytes
+139347102L // the length of the file in bytes
 )
 };
 };
